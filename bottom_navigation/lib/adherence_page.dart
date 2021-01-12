523a6d05feb2126fa81748aea30a706629a8c6db
NipunaCooray <nipuna.cooray@gmail.com>
2021-01-12 11:30:10 +1100

    Simple Bottom Nav

---
+++ bottom_navigation/lib/adherence_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdherencePage extends StatefulWidget {
+  @override
+  _AdherencePageState createState() => _AdherencePageState();
+}
+
+class _AdherencePageState extends State<AdherencePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('This is adherence page'),
+    );
+  }
+}
